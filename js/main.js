83e94a29ec8ea2b1237595a9efe821f919c89894
Freddie Groom <fredgroom@gmail.com>
2025-07-09 13:02:16 +0100

    update render method to allow todo completion

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
   fetchTodos(() => {
     renderTable();
   });
+
+  $('#dataTable').on('click', '.toggle-complete', function () {
+    const row = $(this).closest('tr');
+    const id = parseInt(row.data('id'));
+
+    const todo = todos.find((t) => t.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+      renderTable();
+    }
+  });
 });
--- js/render.js
+++ js/render.js
@@ -1,12 +1,15 @@
 function renderTable() {
   const tbody = $('#dataTable tbody');
+  tbody.empty();
 
   todos.forEach((todo) => {
+    const completedIcon = todo.completed ? 'Yes' : 'No';
     tbody.append(`
-      <tr>
-        <td>${todo.id}</td>
-        <td>${todo.title}</td>
-        <td>${todo.completed ? 'Yes' : 'No'}</td>
-      </tr>`);
+        <tr data-id="${todo.id}">
+          <td>${todo.id}</td>
+          <td>${todo.title}</td>
+          <td class="toggle-complete" style="cursor:pointer;">${completedIcon}</td>
+        </tr>
+      `);
   });
 }
